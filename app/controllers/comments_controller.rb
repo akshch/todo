a3e7fdb32105b9d50f5ffa58bc6b3b9d08ac9d2d
akshch <akshaych2786@gmail.com>
2023-07-22 00:03:31 +0530

    Comment controller added with create action functionality

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,13 @@
+class CommentsController < ApplicationController
+
+  def create
+    @ticket = Ticket.find(params[:ticket_id])
+    @comment = @ticket.comments.create(comment_params)
+    redirect_to ticket_path(@ticket)
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:body, :user_id)
+    end
+end
