e102f0fdb9cd3a69e4ae96974fa3bd6f24929b55
akshch <akshaych2786@gmail.com>
2023-07-24 09:56:16 +0530

    seed data added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+['developer', 'sprint_master', 'admin'].each do |name|
+  User.create(email: "#{name}@gmail.com", password: 'password', role: User.roles["#{name}"])
+end
